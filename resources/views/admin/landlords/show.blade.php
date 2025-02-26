925a685e0cd409c713bf732d779e67ad74ec99b3
Muhammad Arsal-Laptop <charsal13579@gmail.com>
2025-02-26 11:21:38 +0500

    Landlord show page Updated

--- resources/views/admin/landlords/show.blade.php
+++ resources/views/admin/landlords/show.blade.php
@@ -1,11 +1,6 @@
 @extends('admin.partials.main')
 
 @section('css')
-<style>
-    .soft-border {
-        border: 1px solid #e0e0e0 !important; /* Light gray border */
-    }
-</style>
 @endsection
 
 @section('content')
@@ -25,74 +20,73 @@
 
     <div class="content-body">
         <div class="container-fluid p-0 m-0">        
-            <div class="card mb-3">
+            <div class="card">
                 <div class="card-header text-white" style="background-color: #041E41;">
                     <strong>Details</strong>
                 </div>
-                <div class="card-body p-0">
-                    <div class="container-fluid">
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Name:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->title . ' ' . $landlord->name }}</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Mobile phone:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->profile->phone_number ?? 'Not set' }}</div>
-                        </div>
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Work phone:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->work_phone ?? 'Not set' }}</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Home phone:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->home_phone ?? 'Not set' }}</div>
-                        </div>
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Company name:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->company_name ?? 'Not set' }}</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Commission rate:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->commission_rate ? $landlord->commission_rate . '%' : 'Not set' }}</div>
-                        </div>
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">E-mail:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->email ?? 'Not set' }}</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Is active:</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">{{ $landlord->status ? 'Yes' : 'No' }}</div>
-                        </div>
-                    </div>
-                </div>
+                <table class="table table-bordered text-muted mb-0">
+                    <tbody>
+                        <tr>
+                            <th class="font-weight-bold ">Name:</th>
+                            <td class="">{{ $landlord->title . ' ' . $landlord->name }}</td>
+                            <th class="font-weight-bold ">Mobile phone:</th>
+                            <td class="">{{ $landlord->profile->phone_number ?? 'Not set' }}</td>
+                        </tr>
+                        <tr>
+                            <th class="font-weight-bold ">Work phone:</th>
+                            <td class="">{{ $landlord->work_phone ?? 'Not set' }}</td>
+                            <th class="font-weight-bold ">Home phone:</th>
+                            <td class="">{{ $landlord->home_phone ?? 'Not set' }}</td>
+                        </tr>
+                        <tr>
+                            <th class="font-weight-bold ">Company name:</th>
+                            <td class="">{{ $landlord->company_name ?? 'Not set' }}</td>
+                            <th class="font-weight-bold ">Commission rate:</th>
+                            <td class="">{{ $landlord->commission_rate ? $landlord->commission_rate . '%' : 'Not set' }}</td>
+                        </tr>
+                        <tr>
+                            <th class="font-weight-bold ">E-mail:</th>
+                            <td class="">{{ $landlord->email ?? 'Not set' }}</td>
+                            <th class="font-weight-bold ">Is active:</th>
+                            <td class="font-weight-bold ">{{ $landlord->status ? 'Yes' : 'No' }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
+                       
             
             <div class="card mb-3">
                 <div class="card-header text-white" style="background-color: #041E41;">
                     <strong>Address</strong>
                 </div>
-                <div class="card-body p-0">
-                    <div class="container-fluid">
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Line 1:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->line1 ?? 'Not set' }}</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Line 2:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->line2 ?? 'Not set' }}</div>
-                        </div>
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Line 3:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->line3 ?? 'Not set' }}</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">City:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->city }}</div>
-                        </div>
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">County:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->county }}</div>
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Post code:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->postcode }}</div>
-                        </div>
-                        <div class="row soft-border text-muted">
-                            <div class="col-md-3 font-weight-bold soft-border p-1">Country:</div>
-                            <div class="col-md-3 soft-border p-1">{{ $landlord->country ?? 'Not set' }}</div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <table class="table table-bordered text-muted mb-0">
+                    <tbody>
+                        <tr>
+                            <th class="font-weight-bold ">Line 1:</th>
+                            <td class="">{{ $landlord->line1 ?? 'Not set' }}</td>
+                            <th class="font-weight-bold ">Line 2:</th>
+                            <td class="">{{ $landlord->line2 ?? 'Not set' }}</td>
+                        </tr>
+                        <tr>
+                            <th class="font-weight-bold ">Line 3:</th>
+                            <td class="">{{ $landlord->line3 ?? 'Not set' }}</td>
+                            <th class="font-weight-bold ">City:</th>
+                            <td class="">{{ $landlord->city }}</td>
+                        </tr>
+                        <tr>
+                            <th class="font-weight-bold ">County:</th>
+                            <td class="">{{ $landlord->county }}</td>
+                            <th class="font-weight-bold ">Post code:</th>
+                            <td class="">{{ $landlord->postcode }}</td>
+                        </tr>
+                        <tr>
+                            <th class="font-weight-bold ">Country:</th>
+                            <td class="" colspan="3">{{ $landlord->country ?? 'Not set' }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>            
             
-
             <table class="table table-bordered table-striped">
                 <thead class="text-white" style="background-color:  #041E41;">
                     <tr>

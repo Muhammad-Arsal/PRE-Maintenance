41c7a643649e402e0da746b703872cf1d6ed36ce
Muhammad Arsal-Laptop <charsal13579@gmail.com>
2025-03-11 19:14:41 +0500

    Pound sign changed

--- resources/views/admin/properties/show.blade.php
+++ resources/views/admin/properties/show.blade.php
@@ -47,9 +47,9 @@
                         </tr>                        
                         <tr>
                             <th class="font-weight-bold w-25">Weekly rent:</th>
-                            <td class="w-25">{{$property->monthly_rent/4 ?? 'Not Set'}}</td>
+                            <td class="w-25">£ {{$property->monthly_rent/4 ?? 'Not Set'}}</td>
                             <th class="font-weight-bold w-25">Monthly rent:</th>
-                            <td class="w-25">{{$property->monthly_rent ?? 'Not Set'}}</td>
+                            <td class="w-25">£ {{$property->monthly_rent ?? 'Not Set'}}</td>
                         </tr>
                         <tr>
                             <th class="font-weight-bold w-25">Type:</th>
